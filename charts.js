ab20499e35264da7f673fb6256e7114ad1deb409
Will Jones <thefreethinker127@gmail.com>
2015-06-10 17:09:52 -0700

    Added axes and got updating almost working. Need to fix buttons and get new estimates to plot in convergence chart.

--- charts.js
+++ charts.js
@@ -7,45 +7,95 @@
      */
     var plot, dartboard, convergence, xaxis, yaxis, lineFunc;
 
-    var width = 600, height = 300; // Width of graphics
+    var width = 600, height = 300, margin = 40; // Width of graphics
 
     var scale1 = d3.scale.linear().domain([-1,1])
-	.range([0, width]); // Scale for dartboard
+	.range([margin, width + margin]); // Scale for dartboard
 
     // Scales for convergence
-    var scaley = d3.scale.linear().domain([0, 5]).range([0, height]);
+    var scaley = d3.scale.linear().domain([2, 4])
+	.range([height + margin, margin]);
+
+    var scalex;
 
     exports.init = function (n) {
 	// Initializes and renders the first graphics, with n points
 	plot = new Plot();
+	// Add the data to the plots
+	for (var i = 0; i < n; i++) plot.addPoint();
 
 	// Initialize dartboard
 	dartboard = d3.select('.graphic')
-	    .attr({'width': width, 'height': width});
+	    .attr({'width': width + 2 * margin,
+		   'height': width + 2 * margin});
+	var radius = width / 2;
 	dartboard.append('circle')
-	    .attr({'r': scale1(0), 'stroke': 'red', 'fill': 'none',
-		  'cx': width / 2, 'cy': width / 2});
+	    .attr({'r': radius, 'stroke': 'red', 'fill': 'none',
+		  'cx':  radius + margin, 'cy': radius + margin });
 
 	// Initialize convergence
 	convergence = d3.select('.convergencePlot')
-	    .attr({'width': width, 'height': height});
+	    .attr({'width': width + 2 * margin,
+		   'height': height + 2 * margin});
+
 	yaxis = d3.svg.axis().scale(scaley).ticks(4).orient('left');
+	xaxis = d3.svg.axis().scale(scalex);
 
 	convergence.append('line')
-	    .attr({'x1': 0, 'x2': width,
+	    .attr({'x1': margin, 'x2': width + margin,
 		   'y1': scaley(Math.PI), 'y2': scaley(Math.PI),
-		   'stroke-width': 3, 'stroke': 'red'});
+		   'stroke-width': 2, 'stroke': 'red'});
 
+	convergence.append('text')
+	    .attr({'x': width - 200, 'y': 30, 'fill': 'red',
+		  'font-size': '20px'})
+	    .text(Math.PI.toString());
 
-	// Add the data to the plots
-	this.update(n);
+	scalex = d3.scale.linear().domain([0, n])
+	    .range([margin, width + margin]);
+
+	// Add the X Axis
+	xaxis = d3.svg.axis().scale(scalex)
+	    .orient("bottom").ticks(5);
+
+	convergence.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + (height + margin) + ")")
+            .call(xaxis);
+
+	// Add the Y Axis
+	yaxis = d3.svg.axis().scale(scaley)
+	    .orient("left").ticks(5);
+	convergence.append("g")
+            .attr("class", "y axis")
+	    .attr('transform', 'translate(' + margin + ',0)')
+            .call(yaxis);
+
+	lineFunc = d3.svg.line()
+	    .x(function(d, i) { return scalex(i) })
+	    .y(function(d) { return scaley(d) })
+	    .interpolate('linear');
+
+	convergence.append('path')
+	    .attr({'d': lineFunc(plot.estimates),
+		   'stroke': 'blue',
+		   'stroke-width': 2,
+		   'fill': 'none'});
+
+	// Add points to dartboard by simply updating it
+	updateDartboard();
     }
     exports.update = function (n) {
 	// Updates the plots, with n points
 
 	// New points:
 	for (var i = 0; i < n; i++) plot.addPoint();
+	// Update the plots
+	updateDartboard();
+	updateConvergence();
+    }
 
+    function updateDartboard() {
 	// The Dartboard:
 	dartboard.selectAll('circle')
 	    .data(plot.points)
@@ -55,28 +105,18 @@
 		  'cx': function(d){return scale1(d.x) },
 		  'cy': function(d){return scale1(d.y) },
 		  'fill': function(d){return d.inCircle ? 'red' : 'black'} } );
-
-	// The Convergence:
-	var scalex = d3.scale.linear().domain([0, n]).range([0, width]);
-	lineFunc = d3.svg.line()
-	    .x(function(d, i) { return scalex(i) })
-	    .y(function(d) { return scaley(d) })
-	    .interpolate('linear');
-	var xaxis = d3.svg.axis().scale(scalex).tickSize(-height).tickSubdivide(true);	
-	convergence.append('svg:path')
-	    .attr({'d': lineFunc(plot.estimates),
-		   'stroke': 'blue',
-		   'stroke-width': 2,
-		   'fill': 'none'});
-/*	convergence.append('svg:g')
-	    .attr({'class': 'x axis',
-		   'transform': 'translate(0,' + height + ')'})
-	    .call(xaxis);
-	convergence.append('svg:g')
-	    .attr({'class': 'y axis',
-		   'transform': 'translate(-25, 0)'})
-	    .call(yaxis);*/
-
     }
+
+    function updateConvergence() {
+	// The Convergence:
+	scalex.domain([0, plot.estimates.length]);
+	var changes = convergence.transition();
+	changes.select('.line')
+	    .duration(750)
+	    .attr('d', lineFunc(plot.estimates));
+	changes.select('.x.axis')
+	    .duration(750)
+	    .call(xaxis)
+   }
 
 })(this.plotCalc = {});
